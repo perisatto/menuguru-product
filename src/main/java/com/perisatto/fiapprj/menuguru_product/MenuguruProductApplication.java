4075d12f07b30ec8f25aa974f87163dff07fda2a
Rodrigo Perisatto <rodrigo@perisatto.com>
2024-11-16 12:17:38 -0300

    Squashed commit of the following:
    commit a4b127b100a2a8803f55af897585bc9acff8f29d
    Author: Rodrigo Perisatto <rodrigo@perisatto.com>
    Date:   Sat Nov 16 11:49:59 2024 -0300
    
    	All Scenarios
    
    commit 5f5f68f8b1ba6c3fbd15eee6490dc7911c355528
    Author: Rodrigo Perisatto <rodrigo@perisatto.com>
    Date:   Sat Nov 16 11:06:35 2024 -0300
    
    	Minor adjustments
    
    commit c4f674d45cc977061545d386c0a0b7dc136a2543
    Author: Rodrigo Perisatto <rodrigo@perisatto.com>
    Date:   Sat Nov 16 11:05:38 2024 -0300
    
    	First scenario
    
    commit 4b8904d98d4c8e353e83986bcc4ab8760b60eccc
    Author: Rodrigo Perisatto <rodrigo@perisatto.com>
    Date:   Sat Nov 16 10:23:09 2024 -0300
    
    	BDD structure

---
+++ src/main/java/com/perisatto/fiapprj/menuguru_product/MenuguruProductApplication.java
@@ -0,0 +1,13 @@
+package com.perisatto.fiapprj.menuguru_product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenuguruProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenuguruProductApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/perisatto/fiapprj/menuguru_product/bdd/CucumberTest.java
@@ -0,0 +1,12 @@
+package com.perisatto.fiapprj.menuguru_product.bdd;
+
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+public class CucumberTest {
+
+}
